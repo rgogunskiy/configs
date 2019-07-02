ed68010ef6aefe0e991f5a79296d872895d56cf9
Ruslan Gogunskiy <rgogunskiy@gmail.com>
2019-07-02 18:06:38 +0300

    Some update for org-mode

--- emacs/org-mode.el
+++ emacs/org-mode.el
@@ -1,48 +1,55 @@
 (require 'org)
+;; (set-face-attribute 'org-level-1 nil :height 1.0 :background nil)
+(setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
+(setq org-startup-indented t)
+;; (setq org-hide-leading-stars t)
+(setq org-odd-level-only nil) 
 (define-key global-map "\C-cl" 'org-store-link)
 (define-key global-map "\C-ca" 'org-agenda)
-(setq org-log-done t)
-(setq org-agenda-files (list "~/Dropbox/org/work.org"
-			     ))
-
-(setq org-cycle-separator-lines 0)
-
-(setq org-todo-keywords
-      (quote ((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-              (sequence "WAITING(w@/!)" "HOLD(h@/!)" "|" "CANCELLED(c@/!)" "PHONE" "MEETING"))))
-
-(setq org-todo-state-tags-triggers
-      (quote (("CANCELLED" ("CANCELLED" . t))
-              ("WAITING" ("WAITING" . t))
-              ("HOLD" ("WAITING") ("HOLD" . t))
-              (done ("WAITING") ("HOLD"))
-              ("TODO" ("WAITING") ("CANCELLED") ("HOLD"))
-	      ("NEXT" ("WAITING") ("CANCELLED") ("HOLD"))
-              ("DONE" ("WAITING") ("CANCELLED") ("HOLD")))))
-
-(setq org-directory "~/Dropbox/org")
-(setq org-default-notes-file "~/Dropbox/org/refile.org")
-
-;; I use C-c c to start capture mode
 (global-set-key (kbd "C-c c") 'org-capture)
 
-;; Capture templates for: TODO tasks, Notes, appointments, phone calls, meetings, and org-protocol
-;; (setq org-capture-templates
-;;       (quote (("t" "todo" entry (file "~/git/org/refile.org")
-;;                "* TODO %?\n%U\n%a\n" :clock-in t :clock-resume t)
-;; 	      )))
+(setq org-log-done t)
 
-(defun my/org-mode-hook ()
-  "Stop the org-level headers from increasing in height relative to the other text."
-  (dolist (face '(org-level-1
-                  org-level-2
-                  org-level-3
-                  org-level-4
-                  org-level-5))
-    (set-face-attribute face nil :weight 'semi-bold :height 1.0 :background nil)))
+(setq org-default-notes-file (concat org-directory "~/Dropbox/org/inbox.org"))
+(setq org-agenda-files (list "~/Dropbox/org/work.org"
+                             "~/Dropbox/org/inbox.org"
+			     "~/Dropbox/org/personal.org"
+			     "~/Dropbox/org/research.org"
+			     ))
 
 
+(setq org-capture-templates '(("t" "Todo [inbox]" entry
+                               (file+headline "~/Dropbox/org/inbox.org" "Tasks")
+                               "* TODO %i%?")
+			      ("T" "Tickler" entry
+                               (file+headline "~/Dropbox/org/tickler.org" "Tickler")
+                               "* %i%? \n %U")))
+							 
 
+(setq org-refile-targets '((nil :maxlevel . 2)))
 
-(add-hook 'org-mode-hook 'my/org-mode-hook)
+(setq org-agenda-custom-commands 
+      '(("o" "At the work" tags-todo "@work"
+         ((org-agenda-overriding-header "Work")
+          (org-agenda-skip-function #'my-org-agenda-skip-all-siblings-but-first)))
+	("i" "Inbox" tags-todo "@inbox"
+	 ((org-agenda-overriding-header "Inbox")))
+	("r" "Research" tags-todo "@research"
+	 ((org-agenda-overriding-header "Research")))
+	))
 
+(defun my-org-agenda-skip-all-siblings-but-first ()
+  "Skip all but the first non-done entry."
+  (let (should-skip-entry)
+    (unless (org-current-is-todo)
+      (setq should-skip-entry t))
+    (save-excursion
+      (while (and (not should-skip-entry) (org-goto-sibling t))
+        (when (org-current-is-todo)
+          (setq should-skip-entry t))))
+    (when should-skip-entry
+      (or (outline-next-heading)
+          (goto-char (point-max))))))
+		  
+(defun org-current-is-todo ()
+  (string= "TODO" (org-get-todo-state)))
